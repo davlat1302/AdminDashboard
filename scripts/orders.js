8cadef72ca27a59780b1454ab0d417e7e314f67b
davlat1302 <120746897+davlat1302@users.noreply.github.com>
2023-09-25 12:25:21 +0500

    Add files via upload

---
+++ scripts/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName:'JavaScript Tutuorial',
+        productNumber:'85743',
+        paymentStatus:'Due',
+        status:'Pending'
+    },
+    {
+        productName:'CSS Full Curse',
+        productNumber:'972541',
+        paymentStatus:'Refunded',
+        status:'Declined'
+    },
+    {
+        productName:'Flex-Box Tutorial',
+        productNumber:'36452',
+        paymentStatus:'Paid',
+        status:'Active'
+    }
+]
